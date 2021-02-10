b47aec3be441e841e1cd856174cf5fa740194389
Hussein Talal <63732507+husseinTalal2@users.noreply.github.com>
2021-02-11 00:26:14 +0300

    installing and adding firebase functions

---
+++ components/utils/FirebaseFunctions.js
@@ -0,0 +1,87 @@
+import { db, auth, storage } from "./Firebase";
+import { firestore } from "firebase";
+
+export const firebasefunctions = {
+    addPost: addPost,
+    addMember: addMember,
+    getPosts: getPosts,
+    getMembers: getMembers,
+    getMember: getMember,
+    getPost: getPost,
+};
+
+const addPost = (post) => {
+    return db
+        .collection("posts")
+        .doc()
+        .set(post, { merge: true })
+        .then(() => {
+            alert("Your appointment is recorded successfully");
+        })
+        .catch((err) => {
+            alert(err.message);
+        });
+};
+
+const addMember = (member) => {
+    let doc;
+    db.collection("members")
+        .doc()
+        .set(post, { merge: true })
+        .then((docRef) => {
+            doc = docRef;
+            alert("Your appointment is recorded successfully");
+        })
+        .catch((err) => {
+            alert(err.message);
+        });
+    const storageRef = storage.ref("members/" + doc.id);
+    const upload = storageRef.put(member.img);
+    upload.on("state_changed", function complete() {
+        console.log("uploaded");
+    });
+};
+
+const getPosts = async () => {
+    let posts;
+    await db
+        .collection("posts")
+        .get()
+        .then((querySnapshot) => {
+            posts = querySnapshot.docs.map((doc) => doc.data());
+        });
+
+    return posts;
+};
+
+const getMembers = async () => {
+    let members;
+    await db
+        .collection("members")
+        .get()
+        .then((querySnapshot) => {
+            members = querySnapshot.docs.map((doc) => doc.data());
+        });
+
+    return members;
+};
+
+const getPost = async (id) => {
+    let post;
+    await db
+        .collection("posts")
+        .doc(id)
+        .get()
+        .then((data) => (post = data.data()));
+    return post;
+};
+
+const getMember = async (id) => {
+    let member;
+    await db
+        .collection("members")
+        .doc(id)
+        .get()
+        .then((data) => (member = data.data()));
+    return member;
+};
---
+++ firebase.js
@@ -0,0 +1,22 @@
+import * as firebase from "firebase/app";
+import "firebase/analytics";
+import "firebase/auth";
+import "firebase/firestore";
+import "firebase/storage";
+const firebaseConfig = {
+    apiKey: "AIzaSyCLyOz0WdJq5WMGHh69bbVtyc5u1eZjRxA",
+    authDomain: "alrimal-aldhahabiya.firebaseapp.com",
+    projectId: "alrimal-aldhahabiya",
+    storageBucket: "alrimal-aldhahabiya.appspot.com",
+    messagingSenderId: "66187503475",
+    appId: "1:66187503475:web:6fe59dd001c4bf87ee8706",
+    measurementId: "G-6B781076VH",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+//firebase.analytics();
+
+export const db = firebase.firestore();
+export const auth = firebase.auth();
+export const storage = firebase.storage();

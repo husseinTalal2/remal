da655f1938f21d1517ce4139ee4c9324f6d386dd
Hussein Talal <63732507+husseinTalal2@users.noreply.github.com>
2021-02-16 01:37:01 +0300

    gallery pages

---
+++ pages/admin/gallery.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import Footer from "../../components/Footer/Footer";
+import Navbar from "../../components/Navbar/Navbar";
+import { firebaseFunctions } from "../../components/utils/FirebaseFunctions";
+
+function gallery() {
+    const [imgs, setImgs] = useState(null);
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        firebaseFunctions.addGalleryImgs(imgs);
+    };
+    return (
+        <>
+            <Navbar />
+            <div>
+                <div className="flex justify-center items-center my-16">
+                    <h1 className="text-4xl font-bold uppercase">
+                        {" "}
+                        add galley photos
+                    </h1>
+                </div>
+                <div className="flex justify-center items-center ">
+                    <form onSubmit={handleSubmit} className="flex flex-col">
+                        <label className="custom-file-input bg-black text-white px-8 py-4 rounded cursor-pointer">
+                        <input
+                            type="file"
+                            name="imgs"
+                            multiple
+                            onChange={(e) => {
+                                setImgs(e.target.files);
+                            }}
+                            className="p-5 my-5"
+                            required
+                            placeholder="click to choose"
+                            id="imgs-input"
+                        />
+                            Select Images
+                        </label>
+                        
+                        <button type="submit" className="bg-blue-400 mt-8 py-4 rounded">Submit </button>
+                    </form>
+                </div>
+            </div>
+            <Footer />
+        </>
+    );
+}
+
+export default gallery;
--- pages/gallery/index.js
+++ pages/gallery/index.js
@@ -1,38 +1,57 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Footer from "../../components/Footer/Footer";
 import Navbar from "../../components/Navbar/Navbar";
-const imgs = [
-    "https://dummyimage.com/600x360",
-    "https://dummyimage.com/600x360",
-    "https://dummyimage.com/600x360",
-    "https://dummyimage.com/600x360",
-    "https://dummyimage.com/600x360",
-    "https://dummyimage.com/600x360",
-];
+import { firebaseFunctions } from "../../components/utils/FirebaseFunctions";
+import { storage } from "../../firebase";
+// const imgs = [
+//     "https://dummyimage.com/600x360",
+//     "https://dummyimage.com/600x360",
+//     "https://dummyimage.com/600x360",
+//     "https://dummyimage.com/600x360",
+//     "https://dummyimage.com/600x360",
+//     "https://dummyimage.com/600x360",
+// ];
+
+
 function index() {
+    const [imgs, setImgs] = useState([])
+    const fetchImages = async () => {
+        let result = await storage.ref("gallery").listAll();
+        /// map() array of the imageRef.getDownloadURL() promises
+        let urlPromises = result.items.map((imageRef) =>
+            imageRef.getDownloadURL()
+        );
+        // return all resolved promises
+        return Promise.all(urlPromises);
+    };
+
+    useEffect(async () => {
+        const urls = await fetchImages();
+        setImgs(urls);
+    }, []);
     return (
         <>
             <Navbar />
-            <section class="text-gray-600 body-font">
-                <div class="container px-5 py-24 mx-auto">
-                    <div class="flex flex-col text-center w-full mb-20">
-                        <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
+            <section className="text-gray-600 body-font">
+                <div className="container px-5 py-24 mx-auto">
+                    <div className="flex flex-col text-center w-full mb-20">
+                        <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
                             Master Cleanse Reliac Heirloom
                         </h1>
-                        <p class="lg:w-2/3 mx-auto leading-relaxed text-base">
+                        <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
                             Whatever cardigan tote bag tumblr hexagon brooklyn
                             asymmetrical gentrify, subway tile poke
                             farm-to-table. Franzen you probably haven't heard of
                             them man bun deep jianbing selfies heirloom.
                         </p>
                     </div>
-                    <div class="flex flex-wrap -m-4 md:mx-32">
+                    <div className="flex flex-wrap -m-4 md:mx-32">
                         {imgs.map((img) => (
-                            <div class="lg:w-1/3 sm:w-1/2 p-4">
-                                <div class="flex">
+                            <div className="lg:w-1/3 sm:w-1/2 p-4">
+                                <div className="flex">
                                     <img
                                         alt="gallery"
-                                        class=" inset-0 w-full h-full object-cover object-center"
+                                        className=" inset-0 w-full h-64 object-cover object-center"
                                         src={img}
                                     />
                                 </div>

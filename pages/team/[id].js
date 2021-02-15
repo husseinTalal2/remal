939ff7ffeeaded14bcca0ee67d0ddb1ab56354e2
Hussein Talal <63732507+husseinTalal2@users.noreply.github.com>
2021-02-16 01:37:11 +0300

    team member page

--- pages/team/[id].js
+++ pages/team/[id].js
@@ -1,8 +1,10 @@
 import React from "react";
 import Footer from "../../components/Footer/Footer";
 import Navbar from "../../components/Navbar/Navbar";
+import { firebaseFunctions } from "../../components/utils/FirebaseFunctions";
 const member = {
     name: "John Doe",
+    position: "Something",
     bio:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nisl eros, pulvinar facilisis justo mollis, auctor consequat urna. Morbi a bibendum metus. Donec scelerisque sollicitudin enim eu venenatis. Duis tincidunt laoreet ex, in pretium orci vestibulum eget. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Duis pharetra luctus lacus ut vestibulum. Maecenas ipsum lacus, lacinia quis posuere ut, pulvinar vitae dolor. Integer eu nibh at nisi ullamcorper",
     education:
@@ -10,7 +12,19 @@ const member = {
     experience:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nisl eros, pulvinar facilisis justo mollis, auctor consequat urna. Morbi a bibendum metus. Donec scelerisque sollicitudin enim eu venenatis. Duis tincidunt laoreet ex, in pretium orci vestibulum eget. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Duis pharetra luctus lacus ut vestibulum. Maecenas ipsum lacus, lacinia quis posuere ut, pulvinar vitae dolor. Integer eu nibh at nisi ullamcorper",
 };
-function id() {
+
+export const getServerSideProps = async ({query}) => {
+
+
+    const member = await firebaseFunctions.getMember(query.id).then(data => data);
+
+    return {
+        props:{
+            member:member
+        }
+    }
+}
+function id({member}) {
     return (
         <>
             <Navbar />
@@ -18,12 +32,13 @@ function id() {
                 <div className="container flex md:px-36 my-16 justify-center ">
                     <div className="md:w-1/5 flex flex-col items-center">
                         <div className="rounded-full h-40 w-40 flex items-center justify-center overflow-hidden mt-10">
-                            <img src="https://dummyimage.com/600x360" />
+                            <img src={member.img} />
                         </div>
                         <div>
                             <h1 className="text-3xl font-bold mt-4">
                                 {member.name}
                             </h1>
+                            <p className="text-xl mt-2">{member.position}</p>
                         </div>
                     </div>
                     <div className="md:w-4/5 flex flex-col">

eff48b502222fa010682496e04da5784664223c4
Hussein Talal <63732507+husseinTalal2@users.noreply.github.com>
2021-02-16 01:36:11 +0300

    adding more functions

--- components/utils/FirebaseFunctions.js
+++ components/utils/FirebaseFunctions.js
@@ -1,25 +1,28 @@
 import { db, auth, storage } from "../../firebase";
 import { firestore } from "firebase";
+import firebase from "firebase/app";
 const randomNum = () => {
     return Math.random() * 100000;
 };
-const addPost = (post, imgs) => {
+const addPost = async (post, imgs) => {
     imgs = Object.values(imgs);
-    db.collection("posts")
-        .add(post)
-        .then((docRef) => {
-            docRef.set({ id: docRef.id }, { merge: true });
-            imgs.forEach((img) => {
-                const storageRef = storage
-                    .ref()
-                    .child("posts/" + docRef.id + "/" + randomNum());
-                storageRef.put(img);
-            });
-            alert("post added successfully");
+    const docRef = db.collection("posts").doc();
+    docRef.set({ id: docRef.id, imgsUrls:[] }, { merge: true });
+    docRef.set({...post}, {merge: true}).then(() => {
+        alert("added");
+    });
+    imgs.forEach((img) => {
+        const storageRef = storage
+            .ref()
+            .child("posts/" + docRef.id + "/" + randomNum());
+        storageRef.put(img);
+        storageRef.getDownloadURL().then(url => {
+            docRef.update(
+                'imgsUrls', firebase.firestore.FieldValue.arrayUnion(url)
+            );
         })
-        .catch((err) => {
-            alert(err.message);
-        });
+    });
+   
 };
 
 const addMember = async (member, img) => {
@@ -28,12 +31,11 @@ const addMember = async (member, img) => {
         .add(member)
         .then((docRef) => {
             doc = docRef;
-            console.log(doc);
             const storageRef = storage.ref("members/" + doc.id);
             const upload = storageRef.put(img);
             upload.then(function complete() {
                 storageRef.getDownloadURL().then((url) => {
-                    docRef.set({ img: url }, { merge: true });
+                    docRef.set({ img: url, id: docRef.id }, { merge: true });
                 });
             });
             alert("member added successfully");
@@ -87,6 +89,58 @@ const getMember = async (id) => {
     return member;
 };
 
+const getPostImages = async (id) => {
+    let imgs = [];
+    await storage
+        .ref("posts/" + id)
+        .listAll()
+        .then((result) => {
+            result.items.forEach((imageRef) => {
+                // And finally display them
+                imageRef.getDownloadURL().then((url) => imgs.push(url));
+            });
+        })
+        .catch(function (error) {
+            alert(error.message);
+        });
+
+    return imgs;
+};
+
+const getGalleryImgs = async () => {
+    await storage
+    .ref("members")
+    .listAll()
+    .then((result) => {
+        result.items.forEach((imageRef) => {
+            // And finally display them
+            imageRef.getDownloadURL().then((url) => console.log(url));
+        });
+    })
+    .catch(function (error) {
+        alert(error.message);
+    });
+
+    // /// map() array of the imageRef.getDownloadURL() promises
+    //  let urlPromises = result.items.map((imageRef) =>
+    //      imageRef.getDownloadURL().then(url => url)
+    //  );
+    
+    
+}
+
+const addGalleryImgs = (imgs) => {
+    imgs = Object.values(imgs);
+    imgs.forEach((img) => {
+        const storageRef = storage
+            .ref()
+            .child("gallery/" + randomNum() );
+        storageRef.put(img).then(() => {
+            alert("added")
+        }).catch((err) => alert(err.message));
+
+    });
+}
 export const firebaseFunctions = {
     addPost: addPost,
     addMember: addMember,
@@ -94,6 +148,9 @@ export const firebaseFunctions = {
     getMembers: getMembers,
     getMember: getMember,
     getPost: getPost,
+    getPostImages: getPostImages,
+    getGalleryImgs: getGalleryImgs,
+    addGalleryImgs: addGalleryImgs,
     signUp: (email, password) =>
         firebase.auth().createUserWithEmailAndPassword(email, password),
     signIn: (email, password) =>

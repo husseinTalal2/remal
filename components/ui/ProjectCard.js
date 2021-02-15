b516196787573af1c6b7391a3a07c2df6f7ca84a
Hussein Talal <63732507+husseinTalal2@users.noreply.github.com>
2021-02-16 01:35:14 +0300

    projects related

--- components/ui/ProjectCard.js
+++ components/ui/ProjectCard.js
@@ -1,21 +1,35 @@
 import Link from "next/link";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { storage } from "../../firebase";
+import { firebaseFunctions } from "../utils/FirebaseFunctions";
 
 function ProjectCard({ project }) {
+    const [imgs, setImgs] = useState("");
+
+    useEffect(() => {
+        storage
+            .ref("posts/" + project.id)
+            .listAll()
+            .then((result) => {
+                result.items.forEach((imageRef) => {
+                    imageRef.getDownloadURL().then((url) => setImgs(url));
+                });
+            });
+    }, []);
     return (
         <div className="p-4 md:w-1/3 sm:mb-0 mb-6">
             <div className="rounded-lg h-64 overflow-hidden">
                 <img
                     alt="content"
                     className="object-cover object-center h-full w-full"
-                    src={project.img}
+                    src={imgs}
                 />
             </div>
             <h2 className="text-xl font-medium title-font text-white mt-5">
                 {project.title}
             </h2>
             <p className="text-base leading-relaxed mt-2">{project.excerpt}</p>
-            <Link href={project.link}>
+            <Link href={`projects/${project.id}`}>
                 <a className="text-indigo-400 inline-flex items-center mt-3">
                     Learn More
                     <svg
---
+++ pages/projects/[id].js
@@ -0,0 +1,66 @@
+import React, { useEffect, useState } from "react";
+import Footer from "../../components/Footer/Footer";
+import Navbar from "../../components/Navbar/Navbar";
+import { firebaseFunctions } from "../../components/utils/FirebaseFunctions";
+import { storage } from "../../firebase";
+
+export const getServerSideProps = async ({ query }) => {
+    const post = await firebaseFunctions.getPost(query.id).then((data) => data);
+    return {
+        props: {
+            post: post,
+        },
+    };
+};
+
+function post({ post }) {
+    const [imgs, setImgs] = useState("");
+
+    const fetchImages = async () => {
+        let result = await storage.ref("posts/" + post.id).listAll();
+        /// map() array of the imageRef.getDownloadURL() promises
+        let urlPromises = result.items.map((imageRef) =>
+            imageRef.getDownloadURL()
+        );
+        // return all resolved promises
+        return Promise.all(urlPromises);
+    };
+
+    useEffect(async () => {
+        const urls = await fetchImages();
+        setImgs(urls);
+    }, []);
+
+    return (
+        <>
+            <Navbar />
+            <main>
+                <div className="mx-36 my-24">
+                    <div className="imgs flex flex-wrap justify-evenly items-center">
+                        {!!imgs
+                            ? imgs.map((img) => {
+                                  return (
+                                      <div className="w-4/12 max-h-64 overflow-hidden">
+                                          <img
+                                              src={img}
+                                              className="object-fill p-4"
+                                          />
+                                      </div>
+                                  );
+                              })
+                            : ""}
+                    </div>
+                    <div className="flex flex-col justify-center items-start md:mx-32">
+                        <h1 className="text-4xl font-bold my-8">
+                            {post.title}
+                        </h1>
+                        <p className="">{post.body}</p>
+                    </div>
+                </div>
+            </main>
+            <Footer />
+        </>
+    );
+}
+
+export default post;

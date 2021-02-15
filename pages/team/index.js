20d09c478490c8e80641f4da5bc693251a0e26eb
Hussein Talal <63732507+husseinTalal2@users.noreply.github.com>
2021-02-16 01:36:43 +0300

    team members page

--- pages/team/index.js
+++ pages/team/index.js
@@ -1,8 +1,18 @@
+import Link from "next/link";
 import React from "react";
 import Footer from "../../components/Footer/Footer";
 import Navbar from "../../components/Navbar/Navbar";
-
-function index() {
+import MemberCard from "../../components/ui/MemberCard";
+import { firebaseFunctions } from "../../components/utils/FirebaseFunctions";
+export const getServerSideProps = async () => {
+    const members = await firebaseFunctions.getMembers().then((data) => data);
+    return {
+        props: {
+            members: members,
+        },
+    };
+};
+function index({ members }) {
     return (
         <>
             <Navbar />
@@ -20,100 +30,9 @@ function index() {
                         </p>
                     </div>
                     <div className="flex flex-wrap -m-2">
-                        <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
-                            <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-                                <img
-                                    alt="team"
-                                    className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
-                                    src="https://dummyimage.com/80x80"
-                                />
-                                <div className="flex-grow">
-                                    <h2 className="text-gray-900 title-font font-medium">
-                                        Holden Caulfield
-                                    </h2>
-                                    <p className="text-gray-500">UI Designer</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
-                            <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-                                <img
-                                    alt="team"
-                                    className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
-                                    src="https://dummyimage.com/84x84"
-                                />
-                                <div className="flex-grow">
-                                    <h2 className="text-gray-900 title-font font-medium">
-                                        Henry Letham
-                                    </h2>
-                                    <p className="text-gray-500">CTO</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
-                            <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-                                <img
-                                    alt="team"
-                                    className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
-                                    src="https://dummyimage.com/88x88"
-                                />
-                                <div className="flex-grow">
-                                    <h2 className="text-gray-900 title-font font-medium">
-                                        Oskar Blinde
-                                    </h2>
-                                    <p className="text-gray-500">Founder</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
-                            <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-                                <img
-                                    alt="team"
-                                    className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
-                                    src="https://dummyimage.com/90x90"
-                                />
-                                <div className="flex-grow">
-                                    <h2 className="text-gray-900 title-font font-medium">
-                                        John Doe
-                                    </h2>
-                                    <p className="text-gray-500">DevOps</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
-                            <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-                                <img
-                                    alt="team"
-                                    className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
-                                    src="https://dummyimage.com/94x94"
-                                />
-                                <div className="flex-grow">
-                                    <h2 className="text-gray-900 title-font font-medium">
-                                        Martin Eden
-                                    </h2>
-                                    <p className="text-gray-500">
-                                        Software Engineer
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
-                            <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-                                <img
-                                    alt="team"
-                                    className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
-                                    src="https://dummyimage.com/98x98"
-                                />
-                                <div className="flex-grow">
-                                    <h2 className="text-gray-900 title-font font-medium">
-                                        Boris Kitua
-                                    </h2>
-                                    <p className="text-gray-500">
-                                        UX Researcher
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
+                        {members.map((member) => (
+                            <MemberCard member={member} />
+                        ))}
                     </div>
                 </div>
             </section>
